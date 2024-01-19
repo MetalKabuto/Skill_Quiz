6217f960b4f2b4388f1200d419a791286044cfa3
hannes <metalamonite@gmail.com>
2024-01-19 14:36:17 +0100

    Gjort en klass med konstruktor för att göra frågor. Importerat random. Skapat några 'Question' objekt och lagt de i en lista för att testa funktioner. 'pick_Question' väljer en slumpad fråga från listan och lägger dess textvärde i den blå 'frågerutan' i spel-vyn. Tagit bort några utdaterade kommentarer.

--- LIA_Quiz.py
+++ LIA_Quiz.py
@@ -1,4 +1,5 @@
 ﻿import PySimpleGUI as sg
+import random
 sg.set_options(font=("Arial Bold", 16))
 
 main_menu_layout = [
@@ -13,6 +14,7 @@ main_menu_layout = [
     #Row5
     [sg.Button(button_text="Stäng av", size=(10, 1), button_color="#FFFFFF on #150B3F", key="-QUIT-")]
 ]
+
 
 game_layout = [
     #Row 1
@@ -30,7 +32,7 @@ game_layout = [
     #If you make the width bigger than the window size, it doesn't 'spill over'. It stays in the window instead. Height doesn't work that way tough.
     #disabled=True makes it impossible to write in the multiline, but you can still select the text which is not ideal.
     #TODO: Can't figure out a way to disable/hide the scrollbar.
-    [sg.Multiline("Fråga här! \nRad 2 av texten" , size=(720,15), background_color="#150B3F", text_color="#FFFFFF", disabled=True)],
+    [sg.Multiline("Fråga här! \nRad 2 av texten" , size=(720,15), background_color="#150B3F", text_color="#FFFFFF", key="-QUESTIONBOX-" ,disabled=True)],
    
     #Row3
     #default_text needs to be removed manually when typing. Keeping it for now to show what the box is for.
@@ -85,12 +87,10 @@ program_layout = [
     #Columns have to be in a list, otherwise the program crashes on startup.
     #Need to set each columns background_color to match the windows background. Otherwise it will use the default blue for the column.
     #pad attempts to center the elements vertically a bit.
-    #TODO: Can set element justification and alignment here?
     [
         sg.Column(main_menu_layout, key="-COL1-", background_color="#555555", element_justification="center", pad=(0,125)),
         sg.Column(game_layout, visible=False, key="-COL2-", background_color="#555555"),
         sg.Column(settings_layout, visible=False, key="-COL3-", background_color="#555555", element_justification="center", pad=(0,125)),
-        #element_justification="right" makes the button stick to the right side of the screen, like in the wireframe.
         sg.Column(help_layout, visible=False, key="-COL4-", background_color="#555555", element_justification="right")
     ]
 ]
@@ -98,15 +98,59 @@ program_layout = [
 #Settings for the program window.
 window = sg.Window("Game Window", program_layout, size=(800, 800), background_color="#555555", element_justification="center", element_padding=10)
 
+#Class used to create the questions as objects.
+#text = str, solution = str, hints = str (for now), cipher = str, image = str path to the image, cipher_image = same as image, selected = bool, default to False, q_id = int
+class Question:
+    def __init__(self, text, solution, hints, cipher, image, cipher_image, selected, q_id):
+        self.text = text
+        self.solution = solution
+        self.hints = hints
+        self.cipher = cipher
+        self.image = image
+        self.cipher_image = cipher_image
+        self.selected = selected
+        self.q_id = q_id
+
+#Placeholder values while testing question functions
+question1 = Question("Question1 value \nLine 2 test", "Solution here", "Hints here", "Cipher here", "image path here", "cipher image path here", False, 0)
+question2 = Question("Question2 value \nLine fghgfhgfhgfh", "Solution here", "Hints here", "Cipher here", "image path here", "cipher image path here", False, 1)
+question3 = Question("Question3 value \nLine asdasdasdasd", "Solution here", "Hints here", "Cipher here", "image path here", "cipher image path here", False, 2)
+
+#List to loop through to check the available questions.
+question_list = [question1, question2, question3]
+
+#Function had to be placed outside of the Question class, since the question lists are 'list' objects and not 'Question'.
+#Placing the function like this means it can be called without doing 'object_name.pick_Question'.
+#TODO: Program freezes if all questions have been selected. 
+def pick_Question(placeholder_name):
+    #Gets the list length as a value. Subtract 1 since random.randint is inclusive, and len() returns the wrong number.
+    #EG: If the list has 3 elements, len() would return 3. Since list indexes start at 0 and go to len()-1, it wouldn't work without subtracting 1 initially for the question_list_length variable.
+    question_list_length = len(placeholder_name)-1
+    #new_question is used to break the while loop once a new question has been chosen
+    new_question = False
+    while new_question == False:
+        #Generates a random number between and including 0 until the length of the list
+        random_question = random.randint(0, question_list_length)
+        for x in placeholder_name:
+            #Checks that the question's id matches the random number
+            if x.q_id == random_question:
+                #Checks the selected value, since questions should only appear once each.
+                if x.selected == False:
+                    #If everything checks out, the question is added to the display.
+                    window["-QUESTIONBOX-"].update(placeholder_name[random_question].text)
+                    x.selected = True
+                    new_question = True
+                #If the ID and random number match, but the question has been selected already, it breaks and generates a new number:
+                elif x.selected == True:
+                    break
+
 #Makes the program loop, otherwise it closes down upon clicking any button.
 while True:
-    #layout2 används istället för layout, vilket betyder att man kan 'skriva över' innehållet i fönstret för att ändra vy.
-    #window = sg.Window('Game Window', layout2, size=(800, 800), element_justification="center", element_padding=10)
     #TODO: Check if the preset event == 'Exit' is needed. If it isn't, delete it.
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Exit" or event == "-QUIT-":
         break
-    print (event, values)
+    #print (event, values)
    
     #Probably a better way to shift between the menus, but it works for now.
     #COL1 = Main menu, 2 = Game view, 3 = Settings, 4 = Help screen
@@ -117,6 +161,8 @@ while True:
     if event == "-GAMEWINDOW-":
         window["-COL1-"].update(visible=False)
         window["-COL2-"].update(visible=True)
+        #TODO: Change when the game picks a new question, instead of just when you change to the game view.
+        pick_Question(question_list)
     #Settings menu can only be accessed from the main menu, so only those layouts need to be adjusted.
     if event == "-SETTINGS-":
         window["-COL1-"].update(visible=False)
